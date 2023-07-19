18eb8b53d8aacef92509314547333ba5d23b7375
David Liebherr <ambition.aidev@gmail.com>
2023-07-18 20:57:59 -0500

    WIP, created weather fetch fuction for start and end location

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ var startInput = document.getElementById('startLocation')
 var endInput = document.getElementById('endLocation')
 console.log(startInput)
 var searchBtn = document.getElementById('searchBtn')
+var startData = document.getElementById('startWeatherData')
 
 
 // Fetch Weather API for current weather
-function fetchLocationWeather(cityName) {
+function fetchLocationWeather(cityName, elementId) {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey)
         .then(function (response) {
             return response.json()
@@ -14,6 +15,14 @@ function fetchLocationWeather(cityName) {
         .then(function (getWeather) {
             console.log(getWeather)
 
+            var currentTemp = getWeather.main.temp;
+            var maxTemp = getWeather.main.temp_max;
+            var lowTemp = getWeather.main.temp_min;
+            var locationCity = getWeather.name
+            var weatherElement = document.getElementById(elementId)
+                
+                weatherElement.innerHTML = "<h5>City: " + locationCity + "</h5>" + "<li>Current Temp: "  + currentTemp + "</li>" + "<li>High Temp: " + maxTemp + "</li>" + "<li>Low Temp: " + lowTemp + "</li>"
+            console.log(currentTemp)
         })
 
 }
@@ -27,8 +36,8 @@ searchBtn.addEventListener('click', function (event) {
     var startLocationValue = startInput.value
     var endLocationValue = endInput.value
     event.preventDefault()
-    fetchLocationWeather(startLocationValue)
-    fetchLocationWeather(endLocationValue)
+    fetchLocationWeather(startLocationValue, 'startWeatherData')
+    fetchLocationWeather(endLocationValue, 'endWeatherData')
     console.log(startLocationValue, endLocationValue)
 
 })
